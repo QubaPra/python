27973746a826ecebe3cede20fc3fa511a6ddfde5
QubaPra <116064097+QubaPra@users.noreply.github.com>
2023-03-14 14:16:20 +0100

    working download

--- youtube-downloader/youtube.py
+++ youtube-downloader/youtube.py
@@ -1,39 +1,34 @@
 import pytube
 from tkinter import filedialog, Tk
+import os
 
 link = input("Enter the YouTube link: ")
 
 yt = pytube.YouTube(link)
 
 # Prompt the user to select whether to download audio or video
-while True:
-    download_type = input("Do you want to download audio (a) or video (v)? ")
-    if download_type == "a":
-        stream = yt.streams.filter(type="audio").order_by("abr").last()
-        break
-    elif download_type == "v":
-        # Filter and print out only the video streams, sorted by resolution
-        video_streams = yt.streams.filter(type="video").order_by("resolution").desc()
-        stream_info = {}
-        for stream in video_streams:
-            if stream.resolution == "2160p":
-                ext = stream.mime_type.split('/')[-1]
-            elif stream.resolution == "1440p":
-                ext = stream.mime_type.split('/')[-1]
-            else:
-                ext = "mp4"
-            stream_info[f"{stream.resolution} {stream.fps}fps {ext}"] = stream
+download_type = input("Do you want to download audio (a) or video (v)? ")
+audio = yt.streams.filter(type="audio").order_by("abr").last()
 
-        for i, info in enumerate(stream_info.keys()):
-            print(f"{i}: {info}")
+if download_type == "v":
+    # Filter and print out only the video streams, sorted by resolution
+    video_streams = yt.streams.filter(type="video").order_by("resolution").desc()
+    stream_info = {}
+    for stream in video_streams:
+        if stream.resolution == "2160p":
+            ext = stream.mime_type.split('/')[-1]
+        elif stream.resolution == "1440p":
+            ext = stream.mime_type.split('/')[-1]
+        else:
+            ext = "mp4"
+        stream_info[f"{stream.resolution} {stream.fps}fps {ext}"] = stream
 
-        # Prompt the user to select a video stream to download
-        stream_num = int(input("Enter the stream number to download: "))
-        stream = list(stream_info.values())[stream_num]
-        break
+    for i, info in enumerate(stream_info.keys()):
+        print(f"{i}: {info}")
 
-    else:
-        print("Invalid choice. Please enter 'a' or 'v'.")
+    # Prompt the user to select a video stream to download
+    stream_num = int(input("Enter the stream number to download: "))
+    stream = list(stream_info.values())[stream_num]
 
 # Prompt the user to select a download location using a file dialog
 root = Tk()
@@ -41,6 +36,10 @@ root.withdraw()
 download_location = filedialog.askdirectory(title="Choose download location")
 
 # Download the selected audio or video stream
-stream.download(download_location)
+
+audio.download(download_location, filename=audio.default_filename.split('.')[0] + ' audio'+ audio.default_filename[audio.default_filename.rfind("."):])
+
+if (download_type == "v"):
+    stream.download(download_location)
 
 print("Download complete.")
