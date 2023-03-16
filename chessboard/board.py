e5d67a8c23d13799d491748e7c103083cb57bb56
QubaPra <116064097+QubaPra@users.noreply.github.com>
2023-03-16 13:33:19 +0100

    rook added (problems)

--- chessboard/board.py
+++ chessboard/board.py
@@ -26,38 +26,65 @@ class ChessBoard:
     def put_piece(self, piece, row, col):
         self.board[row][col] = piece
     
-    def is_valid_move(self, selected_piece_pos, move_to_pos):
-        # Check if the selected piece is a valid piece
-        selected_piece = self.board[selected_piece_pos[0]][selected_piece_pos[1]]
-        if not selected_piece:
+    def is_valid_move(self, start_pos, end_pos):
+        start_row, start_col = start_pos
+        end_row, end_col = end_pos
+        piece = self.board[start_row][start_col]
+        if not piece:
             return False
+        color, p_type = piece
+        
+        # Check if the move is valid based on the piece type
+        if p_type == "pawn":
+            if color == "white":
+                if start_col == end_col:
+                    if end_row == start_row - 1 and not self.board[end_row][end_col]:
+                        # Move one square forward
+                        return True
+                    elif start_row == 6 and end_row == start_row - 2 and not self.board[end_row][end_col] and not self.board[end_row + 1][end_col]:
+                        # Move two squares forward from the starting position
+                        return True
+                elif abs(end_col - start_col) == 1 and end_row == start_row - 1:
+                    # Capture a piece diagonally
+                    target_piece = self.board[end_row][end_col]
+                    if target_piece and target_piece[0] == "black":
+                        return True
+            elif color == "black":
+                if start_col == end_col:
+                    if end_row == start_row + 1 and not self.board[end_row][end_col]:
+                        # Move one square forward
+                        return True
+                    elif start_row == 1 and end_row == start_row + 2 and not self.board[end_row][end_col] and not self.board[end_row - 1][end_col]:
+                        # Move two squares forward from the starting position
+                        return True
+                elif abs(end_col - start_col) == 1 and end_row == start_row + 1:
+                    # Capture a piece diagonally
+                    target_piece = self.board[end_row][end_col]
+                    if target_piece and target_piece[0] == "white":
+                        return True
+        
+        # Add more code for other piece types...
 
-        # Check if the move is within the bounds of the board
-        if move_to_pos[0] < 0 or move_to_pos[0] > 7 or move_to_pos[1] < 0 or move_to_pos[1] > 7:
+        elif p_type == "rook":
+            # Check if the rook is moving along a row or column
+            if start_row == end_row or start_col == end_col:
+                # Check if there are any pieces blocking the rook's path
+                if start_row == end_row:
+                    # Check the columns between the start and end positions
+                    for col in range(min(start_col, end_col) + 1, max(start_col, end_col)):
+                        if self.board[start_row][col] is not None:
+                            return False
+                else:
+                    # Check the rows between the start and end positions
+                    for row in range(min(start_row, end_row) + 1, max(start_row, end_row)):
+                        if self.board[row][start_col] is not None:
+                            return False
+                # Check if the end position is empty or has an opponent's piece
+                if self.board[end_row][end_col] is None or self.board[end_row][end_col][0] != self.board[start_row][start_col][0]:
+                    return True
             return False
-
-        # Check if the move is valid for the selected piece
-        piece_type = selected_piece[1]
-        if piece_type == "pawn":
-            # ... pawn move validation ...
-            pass
-        elif piece_type == "rook":
-            # ... rook move validation ...
-            pass
-        elif piece_type == "knight":
-            # ... knight move validation ...
-            pass
-        elif piece_type == "bishop":
-            # ... bishop move validation ...
-            pass
-        elif piece_type == "queen":
-            # ... queen move validation ...
-            pass
-        elif piece_type == "king":
-            # ... king move validation ...
-            pass
-
-        return True
+        
+        return False
     
     def move_piece(self, selected_piece_pos, dest_pos):
         if self.is_valid_move(selected_piece_pos, dest_pos):
