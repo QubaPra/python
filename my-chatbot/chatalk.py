900668c9482e9fe63c5491c66ca1b2a9eaac2857
Quba <playquba@gmail.com>
2023-03-24 22:23:00 +0100

    other questions added

--- my-chatbot/chatalk.py
+++ my-chatbot/chatalk.py
@@ -6,6 +6,9 @@ from difflib import get_close_matches
 with open('data.yml', 'r', encoding='utf-8') as f:
     data = yaml.safe_load(f)
 
+with open('other.yml', 'r', encoding='utf-8') as f2:
+    other_data = yaml.safe_load(f2)
+
 while True:
     # Get the input question from the user
     question = input("Ty: ")
@@ -14,7 +17,11 @@ while True:
     best_match = get_close_matches(question, quelist, n=1, cutoff=0.5)
 
     if not best_match:
-        print("Nie znam odpowiedzi na to pytanie :(")
+        best_match = get_close_matches(question, other_data.keys(), n=1, cutoff=0.2)
+        if not best_match:
+            print("Nie znam odpowiedzi na to pytanie :(")
+        else:
+            print(random.choice(other_data[best_match[0]]))
     else:
         for key, patterns in data['questions'].items():
             if best_match[0] in [pattern.lower() for pattern in patterns]:
